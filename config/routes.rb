a1a318c71fb7639ee9d43f31170a2e6b8001ae04
CindyChenx <chen_cx0128@hotmail.com>
2019-10-20 14:23:42 -0400

    finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :coaches
   resources :appointments
   resources :goals
-  resources :users
+  resources :users, accept: [:new]
+
+  get "/signup" => "users#new", as: "signup"
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
